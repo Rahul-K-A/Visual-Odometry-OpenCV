70ecfc941558c447e03f927c178e67f4c299d512
Rahul-K-A <karahul209@gmail.com>
2022-12-21 04:46:10 +0530

    Added README

---
+++ README.md
@@ -0,0 +1,4 @@
+<h1>Visual Odometry in Python</h1>
+
+<p>A small project that tries to replicate the visual odometry process using OpenCV and NumPy.</p>
+<p>Evaluated using the <a href="https://www.cvlibs.net/datasets/kitti/">KITTI dataset.</a></p>
--- VisOdom.py
+++ VisOdom.py
@@ -7,14 +7,21 @@ loadPath="KITTI_sequence_1/"
 calibFile="calib.txt"
 poseFile="poses.txt"
 imagePath="image_l/"
+requiredSize=(618,185)
 
 
 def InitFeatureExtractor():
     orb=cv2.ORB_create()
+    if debug:
+        print(type(orb))
+    
     return orb
     
 
 def loadCalibMatrix():
+    # Loads K and P matrices
+    # K = intrinsic params
+    # P Calibration Matrix
     x=np.loadtxt(loadPath+calibFile)
     output=[]
     for matrix in x:
@@ -25,6 +32,9 @@ def loadCalibMatrix():
     return output
         
 def loadPoses():
+    # Loads pose of camera
+    # Need to stack [0 0 0 1] as told by KITTI since matrix is actually supposed to be 4x4 but represented as 3x4 in file
+
     y=np.loadtxt(loadPath+poseFile)
     output=[]
     for matrix in y:
@@ -45,19 +55,80 @@ def loadImages():
     return list1
 
 def readImage(path):
-    img=cv2.imread(path)
+    # Read image as grayscale
+    img=cv2.imread(path,0)
+    img=cv2.resize(img, dsize=requiredSize )
+    if debug:
+        print(img.shape)
+    return img
+
+
+def showImage(img):
     cv2.imshow("Window",img)
+    cv2.waitKey(1000)
+
+
+def detectFeatures(featureDet:cv2.ORB,img):
+    keyPoints,descriptors=featureDet.detectAndCompute(img,None)
+    return (keyPoints,descriptors)
+    pass
+
+
+
+def compareImages(featureDet,prevImage,currentImage):
+    # Compare images using feature matching
+    # Keep best 20 matches only
+    # Return the coords of matched features
+    
+    output=None
+    prev_img_featurePoints=[]
+    curr_img_featurePoints=[]
+    
+    
+    prevKeyPoints,prevDescriptors=detectFeatures(featureDet,prevImage)
+    currentKeyPoints,currentDescriptors=detectFeatures(featureDet,currentImage)
+    matcher=cv2.BFMatcher()
+    matches=matcher.match(prevDescriptors,currentDescriptors)
+    matches = sorted(matches, key = lambda x:x.distance)
+    matches=matches[:20]
+    output=cv2.drawMatches(prevImage,prevKeyPoints,currentImage,currentKeyPoints,matches,output)
+    showImage(output)
+    for match in matches:
+        prev_img_idx = match.queryIdx
+        curr_img_idx = match.trainIdx
+        
+        img1_Coords=prevKeyPoints[prev_img_idx].pt
+        img2_Coords=currentKeyPoints[curr_img_idx].pt
+        prev_img_featurePoints.append(img1_Coords)
+        curr_img_featurePoints.append(img2_Coords)
+        
+    prev_img_featurePoints=np.asarray(prev_img_featurePoints,dtype=np.float32)
+    curr_img_featurePoints=np.asarray(curr_img_featurePoints,dtype=np.float32)
+    
+    if debug:
+        print(prev_img_featurePoints.shape)
+    
+    return prev_img_featurePoints,curr_img_featurePoints
+    
+
+def findCameraTransform(prevImg_FeatureCoords,currImg_FeatureCoords,K):
+    cv2.findEssentialMat(prevImg_FeatureCoords,currImg_FeatureCoords,K)
         
     
 
 def main():
-    loadCalibMatrix()
-    loadPoses()
+    K,P=loadCalibMatrix()
     featureDet=InitFeatureExtractor()
     imageNames=loadImages()
-    for imageName in imageNames:
-        readImage(loadPath+imagePath+imageName)
-        cv2.waitKey(100)
+    prevImage=readImage(loadPath+imagePath+imageNames[0])
+    
+    for i in range(1,len(imageNames)):
+        img=readImage(loadPath+imagePath+imageNames[i])
+        prevImg_FeatureCoords,currImg_FeatureCoords= compareImages(featureDet,prevImage,img)
+        findCameraTransform(prevImg_FeatureCoords,currImg_FeatureCoords,K)
+        prevImage=img.copy()
+        
+        
         
     
     
